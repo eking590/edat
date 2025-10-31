c603134ff024111f8efaf84e02a85fb49b85025b
ebor odiri kingsley <59615834+eking590@users.noreply.github.com>
2025-10-31 20:57:17 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ MONGO_DB_URI = 'mongodb+srv://edat:Mayorgnn%40088@edat.jotcbls.mongodb.net/produ
 #MONGO_DB_URI = 'mongodb+srv://edatech:vp47FCFbbNUosNED@edat.cjietoh.mongodb.net/production?retryWrites=true&w=majority&appName=Edat'
 #MONGO_DB_URI = 'mongodb+srv://edatech:vp47FCFbbNUosNED@edat.cjietoh.mongodb.net/?retryWrites=true&w=majority&appName=Edat'
 #API_KEY = "dPFNmccRAPS77upmo1mQYcYUFXm3a15z"
-API_KEY = "yia1aigVtSWbrssBKINWyDjtnwRNXem0" 
-#API_KEY = "sk-35d2963f6d6745acb0365be04b9d3063"
+#API_KEY = "yia1aigVtSWbrssBKINWyDjtnwRNXem0" 
+API_KEY = "sk-35d2963f6d6745acb0365be04b9d3063"
 API_NAME= "edatworks"
